4f61fb1c0afb39f5f4bc5b00ba1eebe43f446bd9
Sam Vervaeck <samvv@pm.me>
2024-02-24 21:25:29 +0100

    Add parsers for Linux and fix filetype detection

--- plugin/filetype.lua
+++ plugin/filetype.lua
@@ -14,30 +14,12 @@ local ext_to_ftype = {
   py = 'python',
 }
 
-vim.api.nvim_create_autocmd(
-    {
-        "BufNewFile",
-        "BufRead",
-    },
-    {
-        pattern = "*.tply",
-        callback = function(ev)
-
-          local chunks = split(ev.file, ".")
-          local buf = vim.api.nvim_get_current_buf()
-
-          if #chunks > 2 then
-            local ext = chunks[#chunks-1]
-            local lang = ext_to_ftype[ext]
-            if lang ~= nil then
-              vim.api.nvim_buf_set_option(buf, "filetype", "templaty."..lang)
-              return
-            end
-          end
-
-          -- Only run when nothing matched
-          vim.api.nvim_buf_set_option(buf, "filetype", "templaty")
-        end
-    }
-)
+vim.filetype.add({
+  extension = {
+    tply = "templaty",
+  },
+  pattern = {
+    ['.*.py.tply'] = "templatypython"
+  }
+})
 
